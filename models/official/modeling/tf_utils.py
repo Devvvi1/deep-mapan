6281ca0575db6cdd91254799b5a5ae99216b09f4
Devvvi1 <zixiongchen8@gmail.com>
2022-07-27 03:55:28 +0800

    get config

--- models/official/modeling/tf_utils.py
+++ models/official/modeling/tf_utils.py
@@ -278,9 +278,9 @@ def clone_initializer(initializer):
   # Keras initializer is going to be stateless, which mean reusing the same
   # initializer will produce same init value when the shapes are the same.
   if isinstance(initializer, tf.keras.initializers.Initializer):
+    print("get_config!")
     return initializer.__class__.from_config(initializer.get_config())
   # When the input is string/dict or other serialized configs, caller will
   # create a new keras Initializer instance based on that, and we don't need to
   # do anything
-  print("do anything!")
   return initializer
